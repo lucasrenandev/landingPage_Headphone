508e827359134711b4870ede7cd12064f3bc696f
lucasrenandns <lucasrenandns96@gmail.com>
2022-12-11 20:50:55 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
+// ELEMENTS
 const menuIcon = document.querySelector("#menu-icon")
 const navBar = document.querySelector(".navbar")
 const itemContainer = document.querySelectorAll(".item")
 const productImage = document.querySelector("#product-1")
 
+// MENU 
 menuIcon.addEventListener("click", function() {
     navBar.classList.toggle("open")
     menuIcon.classList.toggle("bx-x")
 })
 
+// CHANGE PRODUCTS
 itemContainer.forEach(item => {
     item.addEventListener("click", function(event) {
         const target = event.currentTarget.id
 
-        if(target === "item1") {
+        if(target === "item-1") {
             productImage.src = "assets/images/product1.png"
         }
-        else if(target === "item2") {
+        else if(target === "item-2") {
             productImage.src = "assets/images/product2.png"
         }
         else {
